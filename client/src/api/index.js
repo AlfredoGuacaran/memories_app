ec82cad76468519aa1fcdaadce8ace718726cff1
AlfredoGuacaran <alfredoguacaran21@gmail.com>
2022-04-15 19:06:59 -0400

    api rute para heroku

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://infinite-wildwood-94097.herokuapp.com/' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
