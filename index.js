1f92dc9844141f31830d7d09ce85c7020cb50810
AlfredoGuacaran <alfredoguacaran21@gmail.com>
2022-04-15 18:57:29 -0400

    reorden carpetas

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
+import path from 'path';
 
 import dotenv from 'dotenv';
 
@@ -15,9 +16,16 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
-app.get('/', (req, res) => {
-  res.send('APP IS RUNNING');
-});
+
+// config static folder
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 //definicion del puerto
 const PORT = process.env.PORT || 5000;
