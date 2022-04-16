de0696a108c511aefc06d90d24120d4632ca51a1
AlfredoGuacaran <alfredoguacaran21@gmail.com>
2022-04-15 23:17:01 -0400

    heroku api ok

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: 'https://infinite-wildwood-94097.herokuapp.com/' });
-
+const API = axios.create({ baseURL: 'https://memoriesappmern.herokuapp.com/' });
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
     req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
